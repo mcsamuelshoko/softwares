b307a25d2760e5ce9199b6355170ffcf9f260128
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-10 10:21:24 +0200

    update shared-constants.ts, added social urls

--- web/src/shared/shared-constants.ts
+++ web/src/shared/shared-constants.ts
@@ -1,3 +1,18 @@
 export const BASE_URL = new URL(
-	"https://mcsamuelshoko.github.io/softwares",
+	"http://localhost:4321"//"https://mcsamuelshoko.github.io/softwares",
 );
+
+export const SOCIAL_URLS = {
+	behance: "https://www.behance.net/mcsamuelshoko",
+	bento: "https://bento.me/mcsamuelshoko",
+	dribbble: "https://dribbble.com/mcsamuelshoko",
+	github: "https://github.com/mcsamuelshoko",
+	linkedIn: "https://www.linkedin.com/in/mc-samuel-shoko-2483731b5/",
+	linkTree: "https://linktr.ee/mcsamuelshoko",
+	microsoftLearn: "https://learn.microsoft.com/en-us/users/mcsamuelshoko-2509/",
+	resume: "https://docs.google.com/document/d/1pyCX2P7chSFPGhTiqSFld0lutiR0GG_oWKL_DN3oMcc/edit?usp=sharing",
+	twitter: "https://x.com/mcsamuelshoko",
+	wakatime: "https://wakatime.com/@ef9df9b6-cb1a-4e73-9d42-35b93e0014d0",
+	x: "https://x.com/mcsamuelshoko",
+
+};
