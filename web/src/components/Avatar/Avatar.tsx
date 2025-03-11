a1271ba4ed13f149502708ad42be17a555413add
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-11 12:32:58 +0200

    finished Avatar.tsx component

--- web/src/components/Avatar/Avatar.tsx
+++ web/src/components/Avatar/Avatar.tsx
@@ -11,9 +11,9 @@ export function Avatar({url,styling}: Readonly<AvatarProps>) {
   return <div>
   <div className="p-2 bg-white rounded-4xl  overflow-y-clip">
     <img
-      src="https://images.pexels.com/photos/29193172/pexels-photo-29193172/free-photo-of-captivating-cherry-blossoms-with-traffic-light-in-japan.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-      alt="a portrait of Mc Samuel Shoko"
-      className={" w-full rounded-3xl object-cover"+ ' '+ (styling ?? 'h-[60vh]')}
+      src={url}
+      alt="avatar"
+      className={" w-full rounded-3xl object-cover"+ ' '+ (styling ?? 'w-full lg:h-[60vh] object-cover')}
     />
   </div>
 </div>
