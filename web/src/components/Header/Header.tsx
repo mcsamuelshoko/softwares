2985896bad98a9ace4be79adfed999c2e0a341e2
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-11 12:36:26 +0200

    styled and added logo to Header.tsx

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
 // import styles from './Header.css';
-
 export interface HeaderProps {
   activeLink: 'home' | 'projects' | 'skills' | 'contact' | 'resume' | 'about',
+  baseUrl:string;
 }
 
-export function Header({ activeLink = 'home' }: Readonly<HeaderProps>) {
+export function Header({ activeLink = 'home',baseUrl }: Readonly<HeaderProps>) {
   const activeLinkStyling: string = 'underline';
 
-  return <div className='flex flex-wrap  justify-center p-4'>
-    <header className={"rounded-full border-4 border-gray-300 p-2 px-12 bg-black text-gray-50 "}>
+
+  return <div className='hidden md:f lex flex-wrap  justify-center p-4'>
+    <header className={"rounded-full border-4 border-gray-300 p-2 px-12 bg-primary backdrop-blur-xl text-white font-display text-xl "}>
       <ul className='flex space-x-4'>
-        <li><a href="/" className={['', (activeLink == 'home' ? activeLinkStyling : '')].join(' ')}>my logo</a></li>
-        <li><a href="#projects" className={['', (activeLink == 'projects' ? activeLinkStyling : '')].join(' ')}>Projects</a></li>
-        <li><a href="#Contact" className={['', (activeLink == 'skills' ? activeLinkStyling : '')].join(' ')}>Contact</a></li>
-        <li><a href="#contact" className={['', (activeLink == 'contact' ? activeLinkStyling : '')].join(' ')}>Contact</a></li>
-        <li><a href="#resume" className={['', (activeLink == 'resume' ? activeLinkStyling : '')].join(' ')}>Resume</a></li>
-        <li><a href="#about" className={['', (activeLink == 'about' ? activeLinkStyling : '')].join(' ')}>About</a></li>
+        <li><a href={baseUrl + "/"} className={['', (activeLink == 'home' ? activeLinkStyling : '')].join(' ')}>
+        <img src={baseUrl+'/logo.svg'} alt="mcsamuel shoko swe logo" className='h-8'/></a></li>
+        <li><a href="#projects" className={['hover:text-accent transition-all', (activeLink == 'projects' ? activeLinkStyling : '')].join(' ')}>Projects</a></li>
+        <li><a href="#skills" className={['hover:text-accent transition-all', (activeLink == 'skills' ? activeLinkStyling : '')].join(' ')}>Skills</a></li>
+        <li><a href="#contact" className={['hover:text-accent transition-all', (activeLink == 'contact' ? activeLinkStyling : '')].join(' ')}>Contact</a></li>
+        <li><a href="#resume" className={['hover:text-accent transition-all', (activeLink == 'resume' ? activeLinkStyling : '')].join(' ')}>Resume</a></li>
+        <li><a href="#about" className={['hover:text-accent transition-all', (activeLink == 'about' ? activeLinkStyling : '')].join(' ')}>About</a></li>
       </ul>
     </header>
   </div>;
