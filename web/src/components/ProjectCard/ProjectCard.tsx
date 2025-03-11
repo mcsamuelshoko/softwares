368c087b183abf2796221085e176c56e48a2b4e6
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-11 12:37:02 +0200

    update ProjectCard.tsx, finished styling and responsiveness on different screens

--- web/src/components/ProjectCard/ProjectCard.tsx
+++ web/src/components/ProjectCard/ProjectCard.tsx
@@ -12,14 +12,14 @@ export interface ProjectCardProps {
 }
 
 export function ProjectCard({ title, description, imageUrl, openLink, moreLink }: Readonly<ProjectCardProps>) {
-  return <div className={'rounded-4xl p-4 flex flex-col justify-between'}>
+  return <div className={'w-80 h-96 border-4 border-gray-100 shadow-sm  bg-primary text-offWhite rounded-2xl md:rounded-3xl lg:rounded-4xl p-4 space-y-4 flex flex-col justify-between'}>
     <div>
-      <img src={imageUrl} alt={title + " project image"} />
-      <p>{title}</p>
-      <p>{description}</p>
+      <img src={imageUrl} alt={title + " project image"} className='rounded-2xl mb-4' />
+      <p className='font-display text-2xl opacity-50  mb-1'>{title}</p>
+      <p className='text-normal '>{description}</p>
     </div>
     <div className='flex justify-between'>
-      <Button title='Open' linkUrl={openLink}></Button>
+      <Button title='Open' color='primary' target='blank' linkUrl={openLink}></Button>
       <Button title='More' linkUrl={moreLink}></Button>
     </div>
   </div>;
