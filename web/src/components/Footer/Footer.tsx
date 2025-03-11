fee8a062bc1dd26a590f36fa5ef4dba4ef215da3
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-11 12:29:56 +0200

    finished Footer.tsx

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -3,19 +3,21 @@ import React from 'react';
 // import styles from './Footer.css';
 
 export interface FooterProps {
+  baseUrl:string;
 }
 
-export function Footer({ }: FooterProps) {
-  return <footer className={''}>
-    <div>
-      <img src="logo" alt="mcsamuel's logo" className='' />
-      <div>
-        <p>Mc Samuel Shoko</p>
-        <p>Thats all folks, thanks for stopping by ❤️</p>
-      </div>
+export function Footer({ baseUrl }: FooterProps) {
+  return <footer className={'bg-primary text-offWhite p-8 text-xl place-content-center'}>
+    <div className=' -mb-36'>
+    <div className='place-items-center'>
+        <p className='text-7xl font-display font-bold opacity-50'>Mc Samuel Shoko</p>
+        <p className='py-8 text-2xl'>Thats all folks!, thanks for stopping by 👋😄</p>
+    </div>
+      <img src={baseUrl+"/favicon.svg"} alt="mcsamuel's logo" className='opacity-30 h-64 relative -left-40 z-20' />
+
     </div>
     <div className='flex flex-col'>
-      <ul className='space-x-4 flex flex-wrap underline'>
+      <ul className='space-x-4 space-y-8 flex  flex-wrap justify-between max-w-4xl mx-auto underline'>
         <li><a href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5" title="visit LinkedIn">Linkedin</a></li>
         <li><a href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">X (Twitter)</a></li>
         <li><a href="https://discord.com/users/generalseven" title="Visit Discord">Discord</a></li>
@@ -24,9 +26,10 @@ export function Footer({ }: FooterProps) {
         <li><a href="https://linktr.ee/mcsamuelshoko" title="visit Linktree profile">Linktree</a></li>
       </ul>
     </div>
-    <div className='h-40'> </div>
-    <div className='p-8'>
+    <div className='h-16'> </div>
+    <div className='text-4xl opacity-50 font-display place-items-center'>
       <p>All rights Reserved &copy; 2023 &mdash; {(new Date().getFullYear())}</p>
     </div>
+    {/* <span className='text-lg'>Made with love ❤️ and passion 🔥 = ❤️‍🔥</span> */}
   </footer>;
 }
