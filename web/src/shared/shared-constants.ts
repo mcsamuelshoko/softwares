9324bb6be84405786ea112bf77328d044e51f16a
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-11 12:34:06 +0200

    update shared-constants.ts, added more links

--- web/src/shared/shared-constants.ts
+++ web/src/shared/shared-constants.ts
@@ -7,13 +7,22 @@ export const SOCIAL_URLS = {
 	bento: "https://bento.me/mcsamuelshoko",
 	discord:"https://discord.com/users/generalseven",
 	dribbble: "https://dribbble.com/mcsamuelshoko",
+	figma: "https://www.figma.com/@mcsamuelshoko",
 	github: "https://github.com/mcsamuelshoko",
-	linkedIn: "https://www.linkedin.com/in/mc-samuel-shoko-2483731b5/",
+	githubRepositories:"https://github.com/mcsamuelshoko?tab=repositories",
+	linkedIn: "https://www.linkedin.com/in/mcsamuelshoko",
 	linkTree: "https://linktr.ee/mcsamuelshoko",
 	microsoftLearn: "https://learn.microsoft.com/en-us/users/mcsamuelshoko-2509/",
+	productHunt:"https://www.producthunt.com/@mcsamuelshoko",
 	resume: "https://docs.google.com/document/d/1pyCX2P7chSFPGhTiqSFld0lutiR0GG_oWKL_DN3oMcc/edit?usp=sharing",
 	twitter: "https://x.com/mcsamuelshoko",
-	wakatime: "https://wakatime.com/@ef9df9b6-cb1a-4e73-9d42-35b93e0014d0",
+	wakatime: "https://wakatime.com/@mcsamuelshoko",
 	x: "https://x.com/mcsamuelshoko",
 
 };
+
+export const CURRENTLY_WORKING_ON = {
+	title:"messenger app",
+	repository:"https://github.com/mcsamuelshoko/telko-moment-mobile",
+	website:"",
+}
