3cefde11e81baf2670077e783e94103aa8e3e3d5
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-11 12:36:03 +0200

    added styling to BigButton.tsx

--- web/src/components/BigButton/BigButton.tsx
+++ web/src/components/BigButton/BigButton.tsx
@@ -10,10 +10,10 @@ export interface BigButtonProps {
 }
 
 export function BigButton({title, link,svgUrl,styling}: BigButtonProps) {
-  return <button type='button' className={''+' '+styling}>{
-    <div className='flex'>
-        <img src={svgUrl} alt="svg icon" className=''/>
-        <p>{title}</p>
+  return <button type='button' className={'bg-primary text-offWhite  rounded-2xl md:rounded-3xl lg:rounded-4xl p-8 px-16 font-medium text-xl'+' '+styling}>{
+    <div className='flex items-center'>
+        <img src={svgUrl} alt="svg icon" className='h-16 invert'/>
+        <p className='text-4xl ml-8'>{title}</p>
     </div>
   }</button>;
 }
