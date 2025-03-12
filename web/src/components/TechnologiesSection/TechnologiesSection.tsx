84229714200032238229f4430a498d982d7be163
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-12 06:12:34 +0200

    center aligned sections for better ux Skills.astro and TechnologiesSection.tsx

--- web/src/components/TechnologiesSection/TechnologiesSection.tsx
+++ web/src/components/TechnologiesSection/TechnologiesSection.tsx
@@ -5,20 +5,24 @@ import React from 'react';
 export interface TechnologiesSectionProps {
   title: string;
   description: string;
-  technologies: { name: string, logoUrl: string,websiteUrl: string }[];
+  technologies: { name: string, logoUrl: string, websiteUrl: string }[];
   animate?: { direction?: 'left' | 'right', duration?: string }
 }
 
 export function TechnologiesSection({ title, description, technologies, animate = { direction: 'right', duration: '10' } }: Readonly<TechnologiesSectionProps>) {
-  return <div className={'space-y-4'}>
-    <p className='text-2xl font-display font-medium opacity-50'>{title}</p>
-    <p className='max-w-xl'>{description}</p>
+  return <div className={'space-y-4 flex flex-col items-center'}>
+    <div className='space-y-4'>
+      <p className='text-2xl font-display font-medium opacity-50'>{title}</p>
+      <p className='max-w-xl'>{description}</p>
+    </div>
     <div>
       <div className='flex flex-wrap overflow-y-hidden space-x-8 space-y-4'>
         {
           technologies.map((tech, index) => <div key={title + index} className=''>
-            <img src={tech.logoUrl} alt={tech.name + ' icon'} title={'visit ' + tech.name} className='h-12 lg:h-16' />
-            {/* {tech.name} */}
+            <a href={tech.websiteUrl} target="_blank" rel="noopener noreferrer"  title={'visit ' + tech.name} className=''>
+              <img src={tech.logoUrl} alt={tech.name + ' icon'} className='h-12 lg:h-16' />
+              {/* {tech.name} */}
+            </a>
           </div>)
         }
       </div>
