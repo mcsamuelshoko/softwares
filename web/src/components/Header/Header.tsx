d6bc4e607be1df347d63d5284556f64464368e5b
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-14 19:00:00 +0200

    made component responsive and mobile friendly,  Header.tsx

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -11,17 +11,18 @@ export function Header({ activeLink = 'home', baseUrl }: Readonly<HeaderProps>) 
   const finalUrl: string = (activeLink === 'home' ? "" : (baseUrl + '/'));
 
   return <header className=' md:flex flex-wrap  place-content-center  md:p-8'>
-    <nav className={"rounded-full border-4 border-gray-300 p-1.5 place-content-center place-items-center px-12 bg-primary backdrop-blur-xl text-white font-display text-xl "}>
-      <ul className='flex flex-wrap space-x-4'>
+    <nav className={" md:rounded-full md:border-4 border-gray-300 p-1.5 place-content-center place-items-center md:px-12 bg-primary1B md:bg-primary backdrop-blur-xl text-white font-display text-lg md:text-xl "}>
+      <ul className='flex flex-wrap justify-center md:space-x-4'>
         <li><a href={baseUrl + "/"} className={['', (activeLink == 'home' ? activeLinkStyling : '')].join(' ')}>
           <img src={baseUrl + '/logo.svg'} alt="mcsamuel shoko swe logo" className='h-8' /></a></li>
-        <li><a href={finalUrl + "#"} className={['hover:text-accent transition-all', (activeLink == 'home' ? activeLinkStyling : '')].join(' ')}>Home</a></li>
-        <li><a href={finalUrl + "#projects"} className={['hover:text-accent transition-all', (activeLink == 'projects' ? activeLinkStyling : '')].join(' ')}>Projects</a></li>
-        <li><a href={finalUrl + "#skills"} className={['hover:text-accent transition-all', (activeLink == 'skills' ? activeLinkStyling : '')].join(' ')}>Skills</a></li>
+        <li><a href={finalUrl + "#"} className={['hover:text-accent transition-all mx-6 md:m-0', (activeLink == 'home' ? activeLinkStyling : '')].join(' ')}>Home</a></li>
+        <li><a href={finalUrl + "#projects"} className={['hover:text-accent transition-all mr-6 md:m-0', (activeLink == 'projects' ? activeLinkStyling : '')].join(' ')}>Projects</a></li>
+        <li><a href={finalUrl + "#skills"} className={['hover:text-accent transition-all hidden md:block', (activeLink == 'skills' ? activeLinkStyling : '')].join(' ')}>Skills</a></li>
         <li><a href={finalUrl + "#contact"} className={['hover:text-accent transition-all', (activeLink == 'contact' ? activeLinkStyling : '')].join(' ')}>Contact</a></li>
-        <li><a href={finalUrl + "#resume"} className={['hover:text-accent transition-all', (activeLink == 'resume' ? activeLinkStyling : '')].join(' ')}>Resume</a></li>
-        <li><a href={finalUrl + "#about"} className={['hover:text-accent transition-all', (activeLink == 'about' ? activeLinkStyling : '')].join(' ')}>About</a></li>
+        <li><a href={finalUrl + "#resume"} className={['hover:text-accent transition-all hidden md:block', (activeLink == 'resume' ? activeLinkStyling : '')].join(' ')}>Resume</a></li>
+        <li><a href={finalUrl + "#about"} className={['hover:text-accent transition-all hidden md:block', (activeLink == 'about' ? activeLinkStyling : '')].join(' ')}>About</a></li>
       </ul>
     </nav>
+
   </header>;
 }
